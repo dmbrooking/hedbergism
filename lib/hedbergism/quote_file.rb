cf8c655ba29ec942ceaf6ecd97e63e5c63ec682c
Dan Brooking <dmbrooking@gmail.com>
2012-05-24 19:37:29 -0400

    Added some command line arguments. Can print usage and pass in custom file.  Also removed README and replaced with .md version

--- lib/hedbergism/quote_file.rb
+++ lib/hedbergism/quote_file.rb
@@ -1,16 +1,25 @@
+require_relative 'quote'
+
 module Hedbergism
   class QuoteFile
     
-    def initialize(filename)
-      @lines = File.readlines(filename)
+    def initialize(file_name)
+      @lines = []
+      File.readlines(file_name).each do |line|
+        addQuote(line)
+      end
+    end
+    
+    def addQuote(line)
+      @lines.push(Quote.new(line))
     end
     
     def firstline
-      @lines[0].chomp
+      @lines[0].to_s
     end
     
     def random
-      @lines.sample
+      @lines.sample.to_s
     end
     
   end
--- spec/hedbergism/quote_file_spec.rb
+++ spec/hedbergism/quote_file_spec.rb
@@ -1,14 +1,13 @@
 require 'hedbergism/quote_file'
 
-describe Hedbergism::QuotesFile do
+describe Hedbergism::QuoteFile do
   before do
     file_name = File.join(File.dirname(__FILE__), '../../bin/quotes.csv')
-    puts file_name
     @quotefile = Hedbergism::QuoteFile.new(file_name)
   end
    
   it "prints 1st line of file" do
-    @quotefile.firstline.should == "I bought a doughnut and they gave me a receipt for the doughnut. I don't need a receipt for the doughnut. I'll just give you the money, and you give me the doughnut, end of transaction. We don't need to bring ink and paper into this. I just can't imagine a scenario where I would have to prove that I bought a doughnut. Some skeptical friend: \"Don't even act like I didn't get that doughnut! I got the documentation right here...oh, wait it's at home...in the file...under \"D\", for \"doughnut.\""
+    @quotefile.firstline.chomp.should == "I bought a doughnut and they gave me a receipt for the doughnut. I don't need a receipt for the doughnut. I'll just give you the money, and you give me the doughnut, end of transaction. We don't need to bring ink and paper into this. I just can't imagine a scenario where I would have to prove that I bought a doughnut. Some skeptical friend: \"Don't even act like I didn't get that doughnut! I got the documentation right here...oh, wait it's at home...in the file...under \"D\", for \"doughnut.\""
   end
   
   
