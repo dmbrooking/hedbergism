0fc19ca4822534c2e73516b5b20712aa8d2b560b
Dan Brooking <dmbrooking@gmail.com>
2012-05-24 20:31:05 -0400

    Guard and Sinatra

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'sinatra'
+
+group :development, :test do
+  gem "rspec"
+  gem "guard"
+  gem "guard-bundler"
+  gem "guard-rspec"
+  gem "growl"
+end
+# gem "rails"
---
+++ hedberg.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+require_relative 'lib/hedbergism/quote_file'
+
+get '/' do
+  content_type :txt
+  
+  file_name = File.join(File.dirname(__FILE__), 'bin/quotes.csv')
+  quotes = Hedbergism::QuoteFile.new(file_name).random
+end
