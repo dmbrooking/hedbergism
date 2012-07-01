feecc73094881e6abb6c0f513900bdda15eb7d04
Dan Brooking <dmbrooking@gmail.com>
2012-07-01 15:47:57 -0400

    Added more facebook like functionality

--- hedberg.rb
+++ hedberg.rb
@@ -56,7 +56,15 @@ __END__
       js.src = "//connect.facebook.net/en_US/all.js";
       ref.parentNode.insertBefore(js, ref);
       }(document));
-    = yield
+    
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=407537325948061";
+      fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+  = yield
 -#   #bg
 -#      %img(src="/images/bg.jpg")
   
